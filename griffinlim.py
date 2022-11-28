756f605b76c79ec378c5773ea3a230898ec10301
filippovillani <filippo.villani.1996@gmail.com>
2022-11-28 20:03:05 +0100

    FGLA implemented

--- griffinlim.py
+++ griffinlim.py
@@ -18,18 +18,24 @@ def griffin_lim_base(spectrogram: np.ndarray,
                      out_path: str, 
                      num_iter: int = 512, 
                      n_fft: int = 1024,
-                     sr: int = 16000):
+                     sr: int = 16000,
+                     init: str = "random"):
     
     out_audio_path = config.RESULTS_DIR / (str(out_path) + '.wav')
-    X_init_phase = np.zeros(spectrogram.shape) # (513, 251)
-    # X_init_phase = np.random.uniform(-np.pi, np.pi, size=X_init_abs.shape)
+    if init == "zeros":
+        X_init_phase = np.zeros(spectrogram.shape)    
+    elif init =="random":
+        X_init_phase = np.random.uniform(-np.pi, np.pi, size=spectrogram.shape)
+    else:
+        raise ValueError("init must be 'zeros' or 'random'")
+    
     X = spectrogram * np.exp(1j * X_init_phase)
     
     for _ in range(num_iter):
         X_hat = librosa.istft(X, n_fft=n_fft)    # G+ cn
-        X_hat = librosa.stft(X_hat, n_fft=n_fft) # G G+ cn # (1025, 126)
-        X_phase = np.angle(X_hat) #(1025, 126)
-        X = spectrogram * np.exp(1j * X_phase)   # Pc1(Pc2(cn-1)) # (513, 251)
+        X_hat = librosa.stft(X_hat, n_fft=n_fft) # G G+ cn  
+        X_phase = np.angle(X_hat) 
+        X = spectrogram * np.exp(1j * X_phase)   # Pc1(Pc2(cn-1))  
     
     x = librosa.istft(X)
     sf.write(out_audio_path, x, samplerate=sr)
@@ -41,26 +47,34 @@ def fast_griffin_lim(spectrogram: np.ndarray,
                     out_path: str, 
                     num_iter: int = 512,
                     alpha: float = 0.99, 
-                    sr: int = 16000):
+                    n_fft: int = 1024,
+                    sr: int = 16000,
+                    init: str = "random"):
 
     out_audio_path = config.RESULTS_DIR / (str(out_path) + '.wav')
-    X_init_abs = np.abs(spectrogram)
-    X_init_phase = np.random.uniform(-np.pi, np.pi, size=X_init_abs.shape)
+    if init == "zeros":
+        X_init_phase = np.zeros(spectrogram.shape)    
+    elif init =="random":
+        X_init_phase = np.random.uniform(-np.pi, np.pi, size=spectrogram.shape)
+    else:
+        raise ValueError("init must be 'zeros' or 'random'")
     
-    cn = X_init_abs * np.exp(1j * X_init_phase)
-    _, t0 = scipy.signal.istft(cn, nperseg=1024, noverlap=256)
-    _, _, t0 = scipy.signal.stft(t0, nperseg=1024, noverlap=256)
-    
-    for n in range(num_iter):
-        _, tn = scipy.signal.istft(cn, nperseg=1024, noverlap=256)
-        _, _, tn = scipy.signal.stft(tn, nperseg=1024, noverlap=256)
-        X_phase = np.angle(tn)
-        tn = X_init_abs * np.exp(1j * X_phase)
+    X = spectrogram * np.exp(1j * X_init_phase)
+    prev_proj = librosa.istft(X, n_fft=n_fft)
+    prev_proj = librosa.stft(prev_proj, n_fft=n_fft)
+    prev_proj_phase = np.angle(prev_proj) 
+    prev_proj = spectrogram * np.exp(1j * prev_proj_phase) 
+    for _ in range(num_iter):
+        curr_proj = librosa.istft(X, n_fft=n_fft)    # G+ cn
+        curr_proj = librosa.stft(curr_proj, n_fft=n_fft) # G G+ cn    
+        curr_proj_phase = np.angle(curr_proj) 
+        curr_proj = spectrogram * np.exp(1j * curr_proj_phase)   # Pc1(Pc2(cn-1))  
         
-        cn = tn + alpha * (tn - t_n_1)
+        X = curr_proj + alpha * (curr_proj - prev_proj)
+        prev_proj = curr_proj
     
-    _, x = scipy.signal.istft(X)
+    x = librosa.istft(X, n_fft=n_fft)
     sf.write(out_audio_path, x, samplerate=sr)
 
     return x
-    
+        
--- main.py
+++ main.py
@@ -6,28 +6,34 @@ import librosa
 import numpy as np
 
 import config
-from griffinlim import griffin_lim_base, griffin_lim_librosa
+from griffinlim import griffin_lim_base, griffin_lim_librosa, fast_griffin_lim
 from plots import plot_reconstructed_audio
 from metrics import si_snr
 
 def main(args):
     snr_path = config.RESULTS_DIR / (args.out_name + '.json')
-    glb_path = args.out_name + 'glb'
+    gla_path = args.out_name + 'glb'
+    fgl_path = args.out_name + 'fgla'
     librosa_path = args.out_name +'librosa'
     with open(config.AUDIO_IN_PATH, 'rb') as f:
         audio, sr = sf.read(f)
     
     # audio = (audio - audio.mean()) / audio.std()
     spectrogram = np.abs(librosa.stft(audio, n_fft=args.n_fft))
-    x_glb = griffin_lim_base(spectrogram, glb_path, args.num_iter, sr=sr, n_fft=args.n_fft)
-    x_libr = griffin_lim_librosa(spectrogram, librosa_path, args.num_iter, sr=sr, n_fft=args.n_fft)
-    plot_reconstructed_audio(audio, x_glb, glb_path)
-    plot_reconstructed_audio(audio, x_libr, librosa_path)
-    snr = {"snr_glb": si_snr(audio, x_glb),
-           "snr_librosa": si_snr(audio, x_libr)}
+    # x_gla = griffin_lim_base(spectrogram, gla_path, args.num_iter, sr=sr, n_fft=args.n_fft)
+    # x_libr = griffin_lim_librosa(spectrogram, librosa_path, args.num_iter, sr=sr, n_fft=args.n_fft)
+    x_fgla = fast_griffin_lim(spectrogram, fgl_path, args.num_iter, sr=sr, n_fft=args.n_fft)
     
-    with open(snr_path, "w") as fp:
-        json.dump(snr, fp)
+    # plot_reconstructed_audio(audio, x_gla, gla_path)
+    # plot_reconstructed_audio(audio, x_libr, librosa_path)
+    plot_reconstructed_audio(audio, x_fgla, fgl_path)
+
+    # snr = {"snr_gla": si_snr(audio, x_gla),
+    #        "snr_fgla": si_snr(audio, x_libr),
+    #        "snr_librosa": si_snr(audio, x_libr)}
+    
+    # with open(snr_path, "w") as fp:
+    #     json.dump(snr, fp)
     
     
 if __name__ == "__main__":
