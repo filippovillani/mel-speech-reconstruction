aec475023b6c0019faa5cc5d4decfc4278eec2db
filippovillani <filippo.villani.1996@gmail.com>
2022-11-29 03:19:41 +0100

    si_snr implemented but results in low scores but audio good

--- metrics.py
+++ metrics.py
@@ -1,15 +1,25 @@
 import numpy as np
+import config
+import soundfile as sf 
 
-def ssnr(s_hat, s_target):
-    s_target = np.divide(np.multiply(np.dot(s_hat, s_target), s_target), np.power(np.abs(s_hat),2))
-    e_noise = s_hat - s_target
-    snr = 10*np.log10(np.sum(np.power(np.abs(s_target), 2)) / np.sum(np.power(np.abs(e_noise), 2)))
+def si_snr(s_target, s_hat): 
+    s_hat = s_hat - np.mean(s_hat)
+    s_target = s_target - np.mean(s_target)
+      
+    s_target_scale= np.dot(s_target, s_hat) / (np.sum(np.power(np.abs(s_target), 2)) + 1e-8)
+    s_target = s_target_scale * s_target
+    
+    target_power = np.sum(np.power(np.abs(s_target),2))
+    disturb_power = np.sum(np.power(np.abs(s_target-s_hat), 2))
+    
+    snr = (target_power / (disturb_power + 1e-8))
+    snr = 10 * np.log10(snr)    
     return snr
 
-def si_snr(s_target, s_hat):
-    s_target = np.divide(np.multiply(np.dot(s_hat, s_target), s_target), np.power(np.abs(s_hat),2))
-    s_targ_power = np.power(np.abs(s_target), 2)
-    e_noise = s_target - s_hat
-    e_noise_power = np.power(np.abs(e_noise), 2)
-    snr = 10 * np.log(np.mean(np.divide(s_targ_power, e_noise_power)))
-    return snr
+if __name__ == "__main__":
+    with open(config.AUDIO_IN_PATH, 'rb') as f:
+        clean, sr = sf.read(f)
+    with open(config.AUDIO_IN_PATH, 'rb') as f:
+        estimated, _ = sf.read(f)
+        
+    snr = si_snr(clean, estimated)
