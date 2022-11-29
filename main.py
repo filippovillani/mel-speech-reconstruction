6daeb60427aadc2583de6be540afbb4ef7a3b6b4
filippovillani <filippo.villani.1996@gmail.com>
2022-11-29 18:24:34 +0100

    main now computes the specified gla type

--- main.py
+++ main.py
@@ -11,30 +11,32 @@ from plots import plot_reconstructed_audio, plot_metric_numiter
 from metrics import si_ssnr
 
 def main(args):
-    results_path = config.RESULTS_DIR / (args.out_name + '.json')
-    gla_path = args.out_name + '_gla'
-    fgla_path = args.out_name + '_fgla'
-    librosa_path = args.out_name +'_librosa'
+    results_path = config.RESULTS_DIR / (args.gla_type + '.json')
+    gla_path = args.gla_type
+
     with open(config.AUDIO_IN_PATH, 'rb') as f:
         audio, sr = sf.read(f)
     
     spectrogram = np.abs(librosa.stft(audio, n_fft=args.n_fft))
-    x_gla, snr_hist_gla = griffin_lim_base(spectrogram, gla_path, args.num_iter, sr=sr, n_fft=args.n_fft)
-    x_fgla, snr_hist_fgla = fast_griffin_lim(spectrogram, fgla_path, args.num_iter, sr=sr, n_fft=args.n_fft)
-    x_libr = griffin_lim_librosa(spectrogram, librosa_path, args.num_iter, sr=sr)
-    
-    plot_metric_numiter(snr_hist_gla, gla_path, "SI-SNR")
-    plot_metric_numiter(snr_hist_fgla, fgla_path, "SI-SNR")
+    if args.gla_type == 'librosa':
+        x_gla = griffin_lim_librosa(spectrogram, gla_path, args.num_iter, sr=sr) 
+        plot_reconstructed_audio(audio, x_gla, gla_path)
+        results = {f"snr_{args.gla_type}": si_ssnr(spectrogram, np.abs(librosa.stft(x_gla, n_fft=args.n_fft)))}
+        with open(results_path, "w") as fp:
+            json.dump(results, fp)        
+        return
+    elif args.gla_type == 'gla':
+        x_gla, snr_hist = griffin_lim_base(spectrogram, gla_path, args.num_iter, sr=sr, n_fft=args.n_fft)
+    elif args.gla_type == 'fgla':
+        x_gla, snr_hist = fast_griffin_lim(spectrogram, gla_path, args.num_iter, sr=sr, n_fft=args.n_fft)
+    else:
+        raise ValueError("gla_type must be one in ['gla', 'fgla', 'librosa']")
     
     plot_reconstructed_audio(audio, x_gla, gla_path)
-    plot_reconstructed_audio(audio, x_libr, librosa_path)
-    plot_reconstructed_audio(audio, x_fgla, fgla_path)
+    plot_metric_numiter(snr_hist, gla_path, "SI-SNR")      
 
-    results = {"snr_gla": si_ssnr(spectrogram, np.abs(librosa.stft(x_gla, n_fft=args.n_fft))),
-               "snr_fgla": si_ssnr(spectrogram, np.abs(librosa.stft(x_fgla, n_fft=args.n_fft))),
-               "snr_librosa": si_ssnr(spectrogram, np.abs(librosa.stft(x_libr, n_fft=args.n_fft))), 
-               "snr_hist_gla": snr_hist_gla,
-               "snr_hist_fgla": snr_hist_fgla}
+    results = {f"snr_{args.gla_type}": si_ssnr(spectrogram, np.abs(librosa.stft(x_gla, n_fft=args.n_fft))),
+               f"snr_hist_{args.gla_type}": snr_hist}
     
     with open(results_path, "w") as fp:
         json.dump(results, fp)
@@ -42,8 +44,9 @@ def main(args):
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--out_name',
+    parser.add_argument('gla_type',
                         type=str,
+                        choices=['gla', 'fgla', 'librosa'],
                         help='name to give to the outputs',
                         default='out')
     parser.add_argument('--num_iter',
