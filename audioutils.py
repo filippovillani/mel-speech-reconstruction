82828add5f22fa1efb6c5ead268cf17aa872de72
filippovillani <filippo.villani.1996@gmail.com>
2022-12-10 16:09:36 +0100

    power_to_db changed to amplitude_to_db, stft changed to spectrogram

--- audioutils.py
+++ audioutils.py
@@ -10,37 +10,24 @@ def melspectrogram(audio: np.ndarray,
                    n_fft: int = 1024, 
                    hop_len: int = 256)->np.ndarray:
     
-    melspectrogram = librosa.power_to_db(librosa.feature.melspectrogram(y=audio,
-                                                                        sr = sr, 
-                                                                        n_fft = n_fft, 
-                                                                        hop_length = hop_len,
-                                                                        n_mels = n_mels), ref=np.max, top_db=80.) / 80. + 1.
+    melspectrogram = librosa.amplitude_to_db(librosa.feature.melspectrogram(y=np.abs(audio),
+                                                                            sr = sr, 
+                                                                            n_fft = n_fft, hop_length = hop_len,
+                                                                            n_mels = n_mels), 
+                                             ref=np.max, 
+                                             top_db=80.) / 80. + 1.
     
     
     return melspectrogram
 
-def stft(audio: np.ndarray,
-         n_fft: int = 1024,
-         hop_len: int = 256)->np.ndarray:
+def spectrogram(audio: np.ndarray,
+                n_fft: int = 1024,
+                hop_len: int = 256)->np.ndarray:
     
-    spectrogram = librosa.stft(y=audio, 
-                               n_fft=n_fft,
-                               hop_length=hop_len)
+    spectrogram = librosa.amplitude_to_db(np.abs(librosa.stft(y=audio, 
+                                                              n_fft=n_fft,
+                                                              hop_length=hop_len)), 
+                                          ref=np.max, 
+                                          top_db=80.) / 80. + 1.
     
     return spectrogram
-
-def compute_mel_spectrogram(stft, hparams):
-    melfb = torch.as_tensor(librosa.filters.mel(sr=hparams.sr, 
-                                                n_fft=hparams.n_fft, 
-                                                n_mels = hparams.n_mels)).to(config.DEVICE)
-    
-    out = torch.empty((hparams.batch_size,
-                       hparams.n_channels,
-                       hparams.n_mels, 
-                       hparams.n_frames))  
-    
-    for n in range(stft.shape[0]):
-        inp = stft[n].squeeze()
-        out[n] = torch.matmul(melfb, inp).unsqueeze(0)
-    
-    return out
