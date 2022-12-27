6d1c75485942a5c38b3fb3454980a330a76ecbb8
filippovillani <filippo.villani.1996@gmail.com>
2022-12-28 00:36:08 +0100

    consistency_projection added

--- networks/DeGLI/layers.py
+++ networks/DeGLI/layers.py
@@ -2,11 +2,12 @@ import torch
 import torch.nn as nn
 
 class DeGLIBlock(nn.Module):
-    def __init__(self):
+    def __init__(self, hparams):
         
         super(DeGLIBlock, self).__init__()
+        self.hprms = hparams
         self.convdnn = ConvDNN()
-        
+    
     def _amplitude_projection(self, stftspec, x_stft = None):
         
         if x_stft is None:
@@ -19,9 +20,12 @@ class DeGLIBlock(nn.Module):
             
         return x_stft
     
-    def _consistency_projection(self):
-        
-        return
+    def _consistency_projection(self, x_amp_proj):
+
+        x_cons_proj = torch.istft(x_amp_proj.permute(0,2,3,1), n_fft=self.hprms.n_fft)    # G+ x
+        x_cons_proj = torch.stft(x_cons_proj, n_fft=self.hprms.n_fft).permute(0,3,1,2) # G G+ x 
+       
+        return x_cons_proj
     
     def forward(self, stftspec, x_stft = None):
         """_summary_
@@ -29,6 +33,7 @@ class DeGLIBlock(nn.Module):
         Args:
             stftspec (torch.Tensor): reference amplitude
             shape=[batch_size, 1, n_stft, n_frames] (1 channel for the real part, one for the imaginary).
+            
             x_stft (torch.Tensor): short-time fourier transform of the signal whose phase is to be reconstructed.
             It is None just for the first block (in this case x_stft=stftspec)  
             shape=[batch_size, 2, n_stft, n_frames] (1 channel for the real part, one for the imaginary).
@@ -37,7 +42,9 @@ class DeGLIBlock(nn.Module):
             _type_: _description_
         """
         x_amp_proj = self._amplitude_projection(stftspec, x_stft)
-        return x_amp_proj
+        x_cons_proj = self._consistency_projection(x_amp_proj)
+        
+        return x_cons_proj
     
 class ConvDNN(nn.Module):  
     def __init__(self):
@@ -46,8 +53,10 @@ class ConvDNN(nn.Module):
         ...
         
 if __name__ == "__main__":
+    from argparse import Namespace
+    hparams = Namespace(n_fft = 1024)
     stftspec = torch.load(r'D:\GitHub_Portfolio\PhaseReconstruction\data\spectrograms\validation\SX445.WAV.wav.pt')
     stftspec = stftspec.unsqueeze(0).unsqueeze(0)
-    model = DeGLIBlock()
+    model = DeGLIBlock(hparams)
     x_stft = model(stftspec)
     x_stft = model(stftspec, x_stft)
