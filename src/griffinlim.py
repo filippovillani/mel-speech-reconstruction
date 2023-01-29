ffad389365eb7454993afd3bfa1cdbd9df059516
filippovillani <filippo.villani.1996@gmail.com>
2023-01-29 15:25:24 +0100

    from numpy to torch

--- src/griffinlim.py
+++ src/griffinlim.py
@@ -1,86 +1,67 @@
-import librosa
-import matplotlib.pyplot as plt
-import numpy as np
-import soundfile as sf
+import torch
+import numpy as torch
+import torch
 
-from metrics import si_sdr_metric
 from utils.audioutils import min_max_normalization
 
 
-def griffin_lim_librosa(spectrogram: np.ndarray, 
-                        n_iter: int = 512, 
-                        init: str = "random"):
+def griffin_lim(spectrogram: torch.Tensor, 
+                n_iter: int = 500, 
+                n_fft: int = 1024,
+                init: str = "zeros"):
     
-    x = librosa.griffinlim(spectrogram, n_iter=n_iter, init=init)      
-
+    X_init_phase = initialize_phase(spectrogram, init)
+    
+    X = spectrogram * torch.exp(1j * X_init_phase)
+    for _ in range(n_iter):
+        X_hat = torch.istft(X, n_fft=n_fft)    # G+ cn
+        X_hat = torch.stft(X_hat, n_fft=n_fft, return_complex = True) # G G+ cn  
+        X_phase = torch.angle(X_hat) 
+        X = spectrogram * torch.exp(1j * X_phase)   # Pc1(Pc2(cn-1))  
+    
+    x = torch.istft(X, n_fft=n_fft)
+    x = min_max_normalization(x)
+    
     return x
 
 
-def griffin_lim_base(spectrogram: np.ndarray, 
-                     n_iter: int = 512, 
+def fast_griffin_lim(spectrogram: torch.Tensor,
+                     n_iter: int = 500,
+                     alpha: float = 0.99, 
                      n_fft: int = 1024,
-                     init: str = "zeros",
-                     eval: bool = False):
-    
-    if init == "zeros":
-        X_init_phase = np.zeros(spectrogram.shape)    
-    elif init =="random":
-        X_init_phase = np.random.uniform(-np.pi, np.pi, size=spectrogram.shape)
-    else:
-        raise ValueError("init must be 'zeros' or 'random'")
-    
-    X = spectrogram * np.exp(1j * X_init_phase)
-    sdr_hist = []
-    for n in range(n_iter):
-        X_hat = librosa.istft(X, n_fft=n_fft)    # G+ cn
-        X_hat = librosa.stft(X_hat, n_fft=n_fft) # G G+ cn  
-        X_phase = np.angle(X_hat) 
-        X = spectrogram * np.exp(1j * X_phase)   # Pc1(Pc2(cn-1))  
-        if eval and n>0:
-            sdr_hist.append(si_sdr_metric(spectrogram, np.abs(X_hat)))
-    
-    x = librosa.istft(X)
-    x = min_max_normalization(x)
-    
-    return x, sdr_hist
+                     init: str = "zeros"):
 
-
-def fast_griffin_lim(spectrogram: np.ndarray,
-                    n_iter: int = 512,
-                    alpha: float = 0.99, 
-                    n_fft: int = 1024,
-                    init: str = "zeros",
-                    eval: bool = False):
-
-    if init == "zeros":
-        X_init_phase = np.zeros(spectrogram.shape)    
-    elif init =="random":
-        X_init_phase = np.random.uniform(-np.pi, np.pi, size=spectrogram.shape)
-    else:
-        raise ValueError("init must be 'zeros' or 'random'")
+    X_init_phase = initialize_phase(spectrogram, init)
     
     # Initialize the algorithm
-    X = spectrogram * np.exp(1j * X_init_phase)
-    prev_proj = librosa.istft(X, n_fft=n_fft)
-    prev_proj = librosa.stft(prev_proj, n_fft=n_fft)
-    prev_proj_phase = np.angle(prev_proj) 
-    prev_proj = spectrogram * np.exp(1j * prev_proj_phase) 
+    X = spectrogram * torch.exp(1j * X_init_phase)
+    prev_proj = torch.istft(X, n_fft=n_fft)
+    prev_proj = torch.stft(prev_proj, n_fft=n_fft, return_complex = True)
+    prev_proj_phase = torch.angle(prev_proj) 
+    prev_proj = spectrogram * torch.exp(1j * prev_proj_phase) 
     
-    sdr_hist = []
-    for n in range(n_iter+1):
-        curr_proj = librosa.istft(X, n_fft=n_fft)    # G+ cn            
-        curr_proj = librosa.stft(curr_proj, n_fft=n_fft) # G G+ cn  
-          
-        if eval and n>0:
-            sdr_hist.append(si_sdr_metric(spectrogram, np.abs(curr_proj)))
-        
-        curr_proj_phase = np.angle(curr_proj) 
-        curr_proj = spectrogram * np.exp(1j * curr_proj_phase)   # Pc1(Pc2(cn-1))  
+    for _ in range(n_iter+1):
+        curr_proj = torch.istft(X, n_fft=n_fft)    # G+ cn            
+        curr_proj = torch.stft(curr_proj, n_fft=n_fft, return_complex = True) # G G+ cn  
+
+        curr_proj_phase = torch.angle(curr_proj) 
+        curr_proj = spectrogram * torch.exp(1j * curr_proj_phase)   # Pc1(Pc2(cn-1))  
             
         X = curr_proj + alpha * (curr_proj - prev_proj)
         prev_proj = curr_proj
 
-    x = librosa.istft(X, n_fft=n_fft)
+    x = torch.istft(X, n_fft=n_fft)
     x = min_max_normalization(x)
 
-    return x, sdr_hist
+    return x
+
+def initialize_phase(spectrogram, init = "zeros"):
+    
+    if init == "zeros":
+        X_init_phase = torch.zeros_like(spectrogram)    
+    elif init =="random":
+        X_init_phase = torch.pi * (2 * torch.rand_like(spectrogram) - 1)
+    else:
+        raise ValueError(f"init must be 'zeros' or 'random', received: {init}")
+    
+    return X_init_phase
