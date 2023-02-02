6e552f58816115e1573edecc0e80ef39851c952a
filippovillani <filippo.villani.1996@gmail.com>
2023-02-02 12:56:48 +0100

    added layer to bottleneck

--- src/networks/PInvModels/models.py
+++ src/networks/PInvModels/models.py
@@ -47,7 +47,7 @@ class PInvConv(nn.Module):
         super(PInvConv, self).__init__()
         self.device = hparams.device
 
-        in_channels = [1] + hparams.conv_channels + hparams.conv_channels[-2::-1]
+        in_channels = [1] + hparams.conv_channels + hparams.conv_channels[-1::-1]
         out_channels = in_channels[1:] + [in_channels[0]]
         
         self.pinvblock = PInvBlock(hparams)
@@ -70,8 +70,7 @@ class PInvConv(nn.Module):
             x = self.convblocks[l](x)
         
         x_max = torch.as_tensor([torch.max(x[q]) for q in range(x.shape[0])])
-        stft_hat = torch.empty(x.shape)
+        stft_hat = torch.empty(x.shape, device=x.device)
         for b in range(stft_hat.shape[0]):
             stft_hat[b] = x[b] / x_max[b]
-        stft_hat = stft_hat.to(x.device)
         return stft_hat
